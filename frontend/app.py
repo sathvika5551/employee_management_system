b154c689145481beea922fbdfc6cd59c3b01dca3
username <useremail>
2026-09-01 19:58:18 +0530

    Connect frontend to backend

--- frontend/app.py
+++ frontend/app.py
@@ -7,7 +7,7 @@ from datetime import date
 # CONFIGURATION
 # =========================================================
 
-API = "http://127.0.0.1:8000"
+API = "https://employee-management-system-pnaj.onrender.com"
 
 st.set_page_config(
     page_title="Employee Management System",
