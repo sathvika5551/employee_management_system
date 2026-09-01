f6bff2d0a9cdc3227bb1d54a0e3b7134eb4fcb17
username <useremail>
2026-09-01 19:05:20 +0530

    Prepare project for deployment

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import mysql.connector
+import os
+from dotenv import load_dotenv
 
 app = FastAPI()
 
@@ -9,12 +11,15 @@ app = FastAPI()
 # DATABASE CONNECTION
 # =========================================================
 
+load_dotenv()
+
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Sathvika@2005",
-        database="employee_management"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT"))
     )
 
 
